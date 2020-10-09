9255f8f870b1fa4cc61c5de59148abecbb244849
dengziqi <1289962514@qq.com>
2020-10-09 15:52:32 +0800

    首尾导航

--- src/main.js
+++ src/main.js
@@ -1,10 +1,30 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+// axios体验
+// import axios from 'axios';
+// // 配置域名
+// axios.defaults.baseURL = 'https://m.maizuo.com/';
+// axios.interceptors.request.use(
+//     function(config) {
+//         config.headers = {
+//             'X-Client-Info':
+//                 '{"a":"3000","ch":"1002","v":"5.0.4","e":"1598087896889693885431809","bc":"110100"}',
+//             'X-Host': 'mall.film-ticket.film.list',
+//         };
+//         // 返回配置
+//         return config;
+//     },
+//     function(err) {
+//         // 对错误的处理
+//     }
+// );
+// axios.get('gateway?cityId=110100&pageNum=1&pageSize=10&type=1&k=5301029');
 
 new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    render: h => h(App),
+}).$mount('#app');
